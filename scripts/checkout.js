0b59fde02ac33e751121e0c909591adc10795e59
Vadim Kim <vkim0203@gmail.com>
2024-08-11 16:47:30 +0900

    Create OOP version of cart

---
+++ scripts/checkout.js
@@ -0,0 +1,8 @@
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
+
+
+renderOrderSummary();
+renderPaymentSummary();
+
