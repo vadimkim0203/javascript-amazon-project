e5875ff788ce25ebba5c46a1b99221277fc13333
Vadim Kim <vkim0203@gmail.com>
2024-11-22 13:27:39 +0900

    commiting the project to remote repository

--- data/products.js
+++ data/products.js
@@ -53,6 +53,32 @@ class Clothing extends Product {
   }
 };
 
+/*const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());*/
+
+/*
+const object2 = {
+  a: 2,
+  b: this.a
+} */
+
+  function logThis() {
+    console.log(this);
+  };
+
+  logThis();
+  logThis.call('hello');
+  
+  
+  const object3 = {
+    method: () => {
+      console.log(this)
+    }
+  }; 
+  object3.method();
+  
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
